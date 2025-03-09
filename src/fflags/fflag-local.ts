a7c56d13c8663bfd31f368fff32ba16dd6cfc1b7
AJ Steinhauser <ajsteinhauser11@gmail.com>
2025-03-08 23:52:47 -0500

    Disabled local plugin

--- src/fflags/fflag-local.ts
+++ src/fflags/fflag-local.ts
@@ -7,10 +7,10 @@ const localFlags: FeatureFlagSchema = {
             localPluginActive: {
                 description:
                     "Due to engine-related bugs involving RunContext: Plugin and Actor:SendMessage, this plugin is currently limited to functioning locally. This flag will disable the locally installed version of the plugin, encouraging users to transition to the Roblox Plugin Manager version. The Plugin Manager version offers enhanced security permissions and a built-in update management system for a more reliable and secure experience.",
-                enabled: true,
+                enabled: false,
                 environmentOverrides: {
-                    development: true,
-                    production: true
+                    development: false,
+                    production: false
                 },
                 createdAt: "Sat Mar  1 08:23:14 EST 2025"
             }
